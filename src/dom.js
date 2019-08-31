03f3710f03e9b7bd91e0c2a07d43501d30b7c06a
Allden <laokahm@gmail.com>
2019-08-31 13:21:28 +0300

    The program is now in a working state, though there's more work to be done. I've added the core functionality such as deleting projects and tasks along with localStorage. There are still a few quirks.

--- src/dom.js
+++ src/dom.js
@@ -1,19 +1,27 @@
 const domBox = () => {
     let projList = document.querySelector('.project-list');
     let taskList = document.querySelector('.task-list');
-
+    
     const appendToProjList = (projName) => {
-        projList.innerHTML += `<li class="project-name">${projName}</li>`;
+        projList.innerHTML += `<div class="project-name"><p>${projName}</p><button class="button delete-project">x</button></div>`;
     }
 
     const appendToTaskList = (title, description, dueDate, priority) => {
         taskList.innerHTML += `
         <div class="task-box">
+            <div class="task-title">
+                <h3>${title}</h3>
+                <div class="title-buttons">
+                    <button class="button display">Display</button>
+                    <button class="button delete-task">Delete</button>
+                </div>
+            </div>
             <ul class="task">
-                <li>Title: ${title}</li>
-                <li>Description: ${description}</li>
-                <li>Date due: ${dueDate}</li>
-                <li>Priority: ${priority}</li>
+                <div class="details">
+                <li><strong>Description</strong>: ${description}</li>
+                <li><strong>Date due</strong>: ${dueDate}</li>
+                <li><strong>Priority</strong>: ${priority}</li>
+                </div>
             </ul>
         </div>
         `;
@@ -23,7 +31,61 @@ const domBox = () => {
         taskList.innerHTML = '';
     }
 
-    return { appendToProjList, appendToTaskList, clearTaskList } 
+    const clearProjList = () => {
+        projList.innerHTML = '';
+    }
+
+    // in this case prompt is the div that is shown and btn is the button that will be clicked
+    const showPrompt = (prompt, btn) => {
+        btn.removeEventListener('click', () => {
+            showPrompt(prompt, btn);
+        });
+        btn.addEventListener('click', () => {
+            hidePrompt(prompt, btn);
+        });
+        prompt.style.display = "block";
+    }
+
+    const hidePrompt = (prompt, btn) => {
+        btn.removeEventListener('click', () => {
+            hidePrompt(prompt, btn);
+        });
+        btn.addEventListener('click', () => {
+            showPrompt(prompt, btn);
+        });
+        prompt.style.display = "none";
+    }
+
+    const showWindowEvent = (prompt, btn) => {
+        btn.addEventListener('click', () => {
+            showPrompt(prompt, btn);
+        });
+    }
+
+    const displayDetailsEvent = () => {
+        let displayButtons = document.getElementsByClassName('display');
+        for(let i = 0; i < displayButtons.length; i++) {
+            displayButtons[i].addEventListener('click', () => displayDetails(i));
+        }
+    }
+
+    const displayDetails = (i) => {
+        let details = document.getElementsByClassName('details');
+        let displayButtons = document.getElementsByClassName('display');
+        displayButtons[i].addEventListener('click', () => hideDetails(i));
+        displayButtons[i].removeEventListener('click', () => displayDetails(i));
+        details[i].style.display = 'block';
+    }
+
+    const hideDetails = (i) => {
+        let details = document.getElementsByClassName('details');
+        let displayButtons = document.getElementsByClassName('display');
+        displayButtons[i].addEventListener('click', () => displayDetails(i));
+        displayButtons[i].removeEventListener('click', () => hideDetails(i));
+        details[i].style.display = 'none';
+    }
+
+    return { appendToProjList, appendToTaskList, clearTaskList, showWindowEvent, displayDetailsEvent, hidePrompt, clearProjList } 
 }
 
 export { domBox }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import * as DOM from './dom.js';
 let domObj = DOM.domBox();
 let projHolder = [];
+let lastClicked = 'default';
 
 const funcBox = () => {
+    let taskPromptBtn = document.querySelector('.show-task-prompt');
+    let taskPromptWindow = document.querySelector('.task-prompt');
+    let newProjectBtn = document.querySelector('.new-project');
+    let projectPromptWindow = document.querySelector('.project-prompt');
+    
     // pushes a  list item into the project with a given name
     const createProject = (projectName) => {
         projHolder.push([projectName]);
-        domObj.appendToProjList(projectName);
-        setEvents();
+        showProjects();
     }
     
     // pushes an object based on the location
@@ -16,43 +21,174 @@ const funcBox = () => {
             if(projHolder[i].indexOf(location) >= 0) {
                 projHolder[i].push({ title, description, dueDate, priority });
                 return;
-            } else if (i === projHolder.length - 1 && projHolder[i].indexOf(location) === -1) {
-                console.log('Project not found, storing in default...');
+            } else if (i === projHolder.length - 1 && projHolder[i].indexOf(location) === -1) {                
                 projHolder[0].push({ title, description, dueDate, priority });
             }
         }
     }
 
-    const setEvents = () => {
+    // add the events for showAttr based on a list of the 'project-name' classes
+    const showAttrEvent = () => {
         let projListItems = document.getElementsByClassName('project-name');
         for(let i = 0; i < projListItems.length; i++) {
             projListItems[i].addEventListener('click', showAttributes);
         }
     }
 
-    const showAttributes = (e) => {
+    // clears the task list each time it's run so that we don't have tasks from different projects contaminating the ui
+    // afterwards iterates over projHolder and compares the first value to the textContent of the button
+    // if it matches it runs appendToTaskList to append the values to the innerHTML of the ul
+    const showAttributes = (e='') => {
         domObj.clearTaskList();
+        if(e!=='') {
+            lastClicked = e.target.childNodes[0].textContent;        
+        }
         for(let i = 0; i < projHolder.length; i++) {
-            if(projHolder[i][0] === e.target.textContent) {
+            if(projHolder[i][0] === lastClicked) {
                 for(let j = 1; j < projHolder[i].length; j++) {
                     domObj.appendToTaskList(projHolder[i][j].title, projHolder[i][j].description, projHolder[i][j].dueDate, projHolder[i][j].priority )
                 }
             }
         }
+        deleteTaskEvent();
+        domObj.displayDetailsEvent();
     }
 
-    return { createTask, createProject }
+    const showProjects = () => {
+        domObj.clearProjList();
+        for(let i = 0; i < projHolder.length; i++) {
+            domObj.appendToProjList(projHolder[i][0]);
+        }
+        showAttrEvent();
+        deleteProjectEvent();
+    }
+
+    // asks the user for the title name and creates a project based on the given name
+    const createProjectEvent = () => {
+        let newProjBtn = document.querySelector('.submit-project');
+        
+        newProjBtn.addEventListener('click', (e) => {
+            let title = document.getElementById('project-title').value;
+            createProject(title)
+            domObj.hidePrompt(projectPromptWindow, newProjectBtn);
+            deleteProjectEvent();
+            e.preventDefault();
+            saveToLocalStorage();
+        });
+    }
+
+    // asks for a series of prompts and then calls the createTask function based on the answers
+    // the location is set to the lastClicked element which is set every time showAttributes is called
+    const createTaskEvent = () => {
+        let newTaskBtn = document.querySelector('.new-task');
+        
+        newTaskBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            // basic variables
+            let title = document.getElementById('title').value;
+            let description = document.getElementById('desc').value;
+            let dueDate = document.getElementById('date').value;
+            let priority = document.getElementById('priority').value;
+            createTask(lastClicked, title, description, dueDate, priority );
+            domObj.appendToTaskList(title, description, dueDate, priority )
+            // after we've submitted our task, the window closes
+            domObj.hidePrompt(taskPromptWindow, taskPromptBtn);
+            domObj.displayDetailsEvent();
+            deleteTaskEvent();
+            saveToLocalStorage();            
+        });
+
+    }
+
+    // iterates over all of our deleteTaskBtn classes and adds an event listener
+    const deleteTaskEvent = () => {
+        let deleteTaskBtn = document.getElementsByClassName('delete-task');
+        for(let i = 0; i < deleteTaskBtn.length; i++) {
+            deleteTaskBtn[i].addEventListener('click', (e) => { 
+                deleteTaskFromUI(e);
+                deleteTaskFromDatabase(e);                
+                saveToLocalStorage();
+            });
+        }
+    }
+
+    // this gets the button's parent Element
+    const deleteTaskFromUI = (e) => {
+        e.target.parentElement.parentElement.parentElement.remove();
+    }
+
+    // this too, but it also gets the first child Node which would be the text
+    const deleteTaskFromDatabase = (event) => {
+        for(let i = 0; i < projHolder.length; i++) {
+            // only iterate over the values if we're in the current project
+            if(projHolder[i][0].includes(lastClicked)) {                
+                for(let j = 0; j < projHolder[i].length; j++) {
+                    // compare the title value of the current iteration to the title in the DOM
+                    if(projHolder[i][j].title === event.target.parentElement.parentElement.childNodes[1].textContent) {
+                        projHolder[i].splice(j, 1);
+                    }
+                }
+            }
+        }
+    }
+
+    const deleteProjectEvent = () => {
+        let deleteProjectBtn = document.getElementsByClassName('delete-project');
+        for(let i = 0; i < deleteProjectBtn.length; i++) {
+            deleteProjectBtn[i].addEventListener('click', (e) => {
+                deleteProjectFromUI(e);
+                deleteProjectFromDatabase(e);                
+                saveToLocalStorage();
+            });
+        }
+    }
+
+    const deleteProjectFromUI = (e) => {
+        e.target.parentElement.remove();
+    }
+
+    const deleteProjectFromDatabase = (e) => {
+        for(let i = 0; i < projHolder.length; i++) {
+            if(projHolder[i][0].includes(e.target.parentElement.childNodes[0].textContent)) {
+                projHolder.splice(i, 1);
+                lastClicked = 'default';
+            }
+        }
+    }
+
+    const saveToLocalStorage = () => {
+        localStorage.setItem('projHolder', JSON.stringify(projHolder));
+        console.log(localStorage.projHolder);
+        console.log(projHolder);
+    }
+
+    const loadLocalStorage = () => {
+        if(localStorage.projHolder) {
+            projHolder = JSON.parse(localStorage.getItem('projHolder'));
+        }
+        console.log(projHolder);
+    }
+
+    const clearLocalStorage = () => {
+        localStorage.clear();
+    }
+
+    const run = () => {
+        loadLocalStorage();
+        createProjectEvent();
+        createTaskEvent();
+        domObj.showWindowEvent(taskPromptWindow, taskPromptBtn);
+        domObj.showWindowEvent(projectPromptWindow, newProjectBtn);
+        // createProject('default');
+        // createTask('default', 'How To Use', 'The + next to "Project List" adds a project, the + next to "Task List" adds a task. If you want to delete a project or a task, press the "x" buttons.', 'N/A', 'High')
+        showProjects();
+        showAttributes();
+        // clearLocalStorage();
+    }
+    
+    return { run }
 }
 
-let myFuncs = funcBox();
-myFuncs.createProject('default');
-myFuncs.createProject('cats');
-myFuncs.createProject('dogs');
-myFuncs.createProject('squids');
 
-myFuncs.createTask('cats', 'feed the cats', 'don\'t forget to feed the cats!', 'yeet', 'high');
-myFuncs.createTask('cats', 'feed the cats', 'don\'t forget to feed the cats!', 'yeet', 'high');
-myFuncs.createTask('cats', 'feed the cats', 'don\'t forget to feed the cats!', 'yeet', 'high');
-myFuncs.createTask('dogs', 'feed the dogs', 'don\'t forget to feed the dogs!', 'yeet', 'high');
-myFuncs.createTask('squids', 'feed the squid', 'don\'t forget to feed the squid!', 'yeet', 'high');
-console.log(projHolder);
+let myFuncs = funcBox();
+myFuncs.run();
